aaca32581737c14ca07e13ee301b2e2c277403d8
Paulo <paulituvitu@hotmail.com>
2020-07-07 10:35:25 -0300

    Login page

--- Frontend/src/components/login/index.tsx
+++ Frontend/src/components/login/index.tsx
@@ -5,8 +5,30 @@ import './style.scss'
 export default class Login extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Login</h1>
+			<div className="login-container">
+				<div className="content">
+
+					<header className="header">
+						<img src="" alt=""/>
+					</header>
+
+						<form onSubmit={ ()=>{} }>
+							<h1>Login do usuário</h1>
+							<fieldset>
+								<label htmlFor="email">Email</label>
+								<br/>
+								<input type="text" name="email" placeholder="Seu email"/>
+							</fieldset>
+
+							<fieldset>
+								<label htmlFor="password">Senha</label>
+								<br/>
+								<input type="password" name="password" placeholder="Senha"/>
+							</fieldset>
+
+							<button type="submit" className="btn-default">Login</button>
+						</form>
+				</div>
 			</div>
 		)
 	}
